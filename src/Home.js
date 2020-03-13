02d222842c1a6095d6d68421fbff77bb0e6485b2
ziyeheimao <ziyeheimaoyuye@sina.com>
2020-03-13 13:32:28 +0800

    03-13

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 // import ReactDOM from 'react-dom'
 
 class Home extends React.Component{
@@ -6,6 +7,16 @@ class Home extends React.Component{
     return(
       <div>
         <div>Home</div>
+
+        <Link to='Page1'>
+          <div>Page1</div>
+        </Link>
+        <Link to='Page2'>
+          <div>Page2</div>
+        </Link>
+        <Link to='Page3'>
+          <div>Page3</div>
+        </Link>
       </div>
     )
   }
--- src/Page1.js
+++ src/Page1.js
@@ -1,11 +1,17 @@
 import React from 'react'
 // import ReactDOM from 'react-dom'
+import './style.css'
 
+let style = {
+  backgroundColr: 'red', // - 变驼峰
+  outline: '1px solid red',
+  padding: '20px'
+}
 class Page1 extends React.Component{
   render(){
     return(
       <div>
-        <div>Page1</div>
+        <div className='asd' style={style}>Page1</div>
       </div>
     )
   }
